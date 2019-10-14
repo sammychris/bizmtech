2acc8dec809f9ef876968120efc4e2de7be34931
samuel <ebusameric@gmail.com>
2019-10-14 17:56:40 +0100

    features: add header nav bar

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header id="header">
+		  <nav id="navbar">
+		    <ul>
+		      <li><a href="#welcome-section">Tips on how to grow you business</a></li>
+		      <li><a href="#contacts">Contact me</a></li>
+		    </ul>
+		  </nav>
+		</header>
+	);
+}
+
+export default Header;
