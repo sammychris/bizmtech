e9011e74666f1bac8635fc7cd81beb4d124f383e
samuel <ebusameric@gmail.com>
2019-10-16 18:04:46 +0100

    features: add a welcome banner section

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Banner() {
+	return (
+		<section id="welcome-banner">
+		  	<div>
+		  		<h1>Hey i'am Samuel</h1>
+		    	<p>a web developer</p>
+		  	</div>
+	  	</section>
+	);
+}
+
+export default Banner;
