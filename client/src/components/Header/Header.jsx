2c08c6e68c74d003e9a71bd2e97c85bb555a1d43
samuel <ebusameric@gmail.com>
2019-10-17 18:12:07 +0100

    fix

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
 		<header id="header">
 		  <nav id="navbar">
 		    <ul>
-		      <li><a href="#welcome-section">Tips on how to grow you business</a></li>
+		      <li><a href="#welcome-section">Tips on how to grow your business</a></li>
 		      <li><a href="#contacts">Contact me</a></li>
 		    </ul>
 		  </nav>
