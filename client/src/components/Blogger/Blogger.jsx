75f78868d930d39486f1481ecf5e1818e493dabd
samuel <ebusameric@gmail.com>
2019-10-18 18:12:37 +0100

    features: add a blog section

---
+++ client/src/components/Blogger/Blogger.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Blogger() {
+	return (
+		<section id="blog">
+			<header>
+				<h2>Blog..</h2>
+			</header>
+			<ul>
+				<li class="project-tile">
+					<a href="https://codepen.io/SammyChris/pen/drdwdJ">
+						<img src="https://i.postimg.cc/nV7424wx/Capture.jpg" alt="" />
+						<p>Product Landing Page</p>
+					</a>
+				</li>
+				<li class="project-tile">
+					<a href="https://codepen.io/SammyChris/full/RdQVQw">
+						<img src="https://i.postimg.cc/cCbGrqyq/Capture2.jpg" alt="" />
+						<p>Survey Form</p>
+					</a>
+				</li>
+				<li class="project-tile">
+					<a href="https://codepen.io/SammyChris/full/wOyYEz">
+						<img src="https://i.postimg.cc/jjPfcKZX/Capture3.jpg" alt="" />
+						<p>Tribute Page</p>
+					</a>
+				</li>
+				<li class="project-tile">
+					<a href="https://codepen.io/SammyChris/full/JzLbyZ">
+						<img src="https://i.postimg.cc/3rvwD9fX/Capture4.jpg" alt="" />
+						<p>Javascript Documentation Page</p>
+					</a>
+				</li>
+			</ul>
+	  	</section>
+	);
+}
+
+export default Blogger;
