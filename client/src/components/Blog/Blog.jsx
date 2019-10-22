fb12cb6294a7c56ed7b4d7df8351a0a56ee1e28c
samuel <ebusameric@gmail.com>
2019-10-22 14:37:43 +0100

    features: modifying

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 
-function Blogger() {
+function Blog() {
 	return (
 		<section id="blog">
-			<header>
-				<h2>Blog..</h2>
-			</header>
 			<ul>
 				<li class="project-tile">
 					<a href="https://codepen.io/SammyChris/pen/drdwdJ">
 						<img src="https://i.postimg.cc/nV7424wx/Capture.jpg" alt="" />
-						<p>Product Landing Page</p>
+						<p>How to grow you coffee shop online with 5 steps</p>
 					</a>
 				</li>
 				<li class="project-tile">
 					<a href="https://codepen.io/SammyChris/full/RdQVQw">
 						<img src="https://i.postimg.cc/cCbGrqyq/Capture2.jpg" alt="" />
-						<p>Survey Form</p>
+						<p>How to grow you coffee shop online with 5 steps</p>
 					</a>
 				</li>
 				<li class="project-tile">
 					<a href="https://codepen.io/SammyChris/full/wOyYEz">
 						<img src="https://i.postimg.cc/jjPfcKZX/Capture3.jpg" alt="" />
-						<p>Tribute Page</p>
+						<p>How to grow you coffee shop online with 5 steps</p>
 					</a>
 				</li>
 				<li class="project-tile">
 					<a href="https://codepen.io/SammyChris/full/JzLbyZ">
 						<img src="https://i.postimg.cc/3rvwD9fX/Capture4.jpg" alt="" />
-						<p>Javascript Documentation Page</p>
+						<p>How to grow you coffee shop online with 5 steps</p>
 					</a>
 				</li>
 			</ul>
@@ -36,4 +33,4 @@ function Blogger() {
 	);
 }
 
-export default Blogger;
+export default Blog;
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Banner from '../Banner';
+import Blog from '../Blog';
 
 function Main() {
 	return (
 		<main id="main">
+			<Banner />
+			<Blog />
+
 		</main>
 	);
 }
