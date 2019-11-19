a6d8bcea924a66aed78e54c7a7476599abb8e475
unknown <ebusameric@gmail.com>
2019-11-19 16:44:29 +0100

    features: make some changes to banner section

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -3,29 +3,27 @@ import React from 'react';
 function Blog() {
 	return (
 		<section id="blog">
+			<h2>Our Blog</h2>
 			<ul>
 				<li class="project-tile">
+					<img src="/images/blogimg.jpg" alt="" />
+					<p>How to grow you coffee shop online with 5 steps</p>
 					<a href="https://codepen.io/SammyChris/pen/drdwdJ">
-						<img src="https://i.postimg.cc/nV7424wx/Capture.jpg" alt="" />
-						<p>How to grow you coffee shop online with 5 steps</p>
+						<button>Read the Article</button>
 					</a>
 				</li>
 				<li class="project-tile">
+					<img src="/images/blogimg.jpg" alt="" />
+					<p>How to grow you coffee shop online with 5 steps</p>
 					<a href="https://codepen.io/SammyChris/full/RdQVQw">
-						<img src="https://i.postimg.cc/cCbGrqyq/Capture2.jpg" alt="" />
-						<p>How to grow you coffee shop online with 5 steps</p>
+						<button>Read the Article</button>
 					</a>
 				</li>
 				<li class="project-tile">
+					<img src="/images/blogimg.jpg" alt="" />
+					<p>How to grow you coffee shop online with 5 steps</p>
 					<a href="https://codepen.io/SammyChris/full/wOyYEz">
-						<img src="https://i.postimg.cc/jjPfcKZX/Capture3.jpg" alt="" />
-						<p>How to grow you coffee shop online with 5 steps</p>
-					</a>
-				</li>
-				<li class="project-tile">
-					<a href="https://codepen.io/SammyChris/full/JzLbyZ">
-						<img src="https://i.postimg.cc/3rvwD9fX/Capture4.jpg" alt="" />
-						<p>How to grow you coffee shop online with 5 steps</p>
+						<button>Read the Article</button>
 					</a>
 				</li>
 			</ul>
