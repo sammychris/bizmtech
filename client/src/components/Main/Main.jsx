dd12cf167e0a95e75969b82221080a7b1c013b95
unknown <ebusameric@gmail.com>
2019-12-02 16:49:54 +0100

    features: add some changes to main sections

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Banner from '../Banner';
 import Blog from '../Blog';
+import Services from '../Services';
+import Portfolio from '../Portfolio';
 
 function Main() {
 	return (
 		<main id="main">
 			<Banner />
 			<Blog />
-
+			<Services />
+			<Portfolio />
 		</main>
 	);
 }
