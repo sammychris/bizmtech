c093136f91b9afcf9489f729804a8f00e6439685
unknown <ebusameric@gmail.com>
2019-12-05 16:52:06 +0100

    features: add some changes to services sections

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -3,33 +3,37 @@ import React from 'react';
 function Services() {
 	return (
 		<section id="services">
-			<header>
-				<h2>This is how I can help you get more customers/clients</h2>
-			</header>
+			<h2>This is how We can help you get more customers and clients</h2>
 			<ul>
 				<li class="project-tile">
-					<a href="https://codepen.io/SammyChris/pen/drdwdJ">
-						<img src="https://i.postimg.cc/nV7424wx/Capture.jpg" alt="" />
-						<p>Product Landing Page</p>
-					</a>
+					<span><i className="fas fa-vector-square"></i></span>
+					<h3>Web Development</h3>
+					<p>We will design website for your business to give you an online presents, for your small or big business your customers needs to know how they can reach you.</p>
 				</li>
 				<li class="project-tile">
-					<a href="https://codepen.io/SammyChris/full/RdQVQw">
-						<img src="https://i.postimg.cc/cCbGrqyq/Capture2.jpg" alt="" />
-						<p>Survey Form</p>
-					</a>
+					<span><i className="fas fa-search-location"></i></span>
+					<h3>SEO</h3>
+					<p>Search engine optimization (SEO) is the process of increasing the quality and quantity of website traffic by increasing the visibility of a website or a web page to users of a web search engine.</p>
 				</li>
 				<li class="project-tile">
-					<a href="https://codepen.io/SammyChris/full/wOyYEz">
-						<img src="https://i.postimg.cc/jjPfcKZX/Capture3.jpg" alt="" />
-						<p>Tribute Page</p>
-					</a>
+					<span><i className="fab fa-google-drive"></i></span>
+					<h3>Adwords</h3>
+					<p>AdWords (Google AdWords) is an advertising service by Google for businesses wanting to display ads on Google and its advertising network. The AdWords program enables businesses to set a budget for advertising and only pay when people click the ads.</p>
 				</li>
 				<li class="project-tile">
-					<a href="https://codepen.io/SammyChris/full/JzLbyZ">
-						<img src="https://i.postimg.cc/3rvwD9fX/Capture4.jpg" alt="" />
-						<p>Javascript Documentation Page</p>
-					</a>
+					<span><i className="fas fa-map-marker-alt"></i></span>
+					<h3>Add to Google Map</h3>
+					<p>Google Maps is a great business weapon for small to medium sized companies. It helps people locate your office and tell them that there is a company that can ...</p>
+				</li>
+				<li class="project-tile">
+					<span><i className="fas fa-hashtag"></i></span>
+					<h3>Social Media</h3>
+					<p>Social media offers an easy and low-commitment way for potential customers to express interest in your business and your products. Lead generation is such an important benefit of social media for business that many social networks offer advertising formats specifically designed to collect leads.</p>
+				</li>
+				<li class="project-tile">
+					<span><i className="fas fa-ad"></i></span>
+					<h3>Advertising</h3>
+					<p>Online advertising, also called online marketing or Internet advertising or web advertising, is a form of marketing and advertising which uses the Internet to deliver promotional marketing messages to</p>
 				</li>
 			</ul>
 	  	</section>
