07a3863861718b82c1961052bef434d6fabd1052
unknown <ebusameric@gmail.com>
2019-12-09 16:57:25 +0100

    features: add mail functionality module

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import MailSettings from './MailSettings';
 
 export {
 	Header,
 	Main,
 	Footer,
+	MailSettings,
 }
