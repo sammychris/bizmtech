1b0c77fbe5930c6741e75281e0fd4c3d59368568
unknown <ebusameric@gmail.com>
2019-12-10 16:58:09 +0100

    features: send mail functionality complete

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import MailSettings from './MailSettings';
+import SendMailForm from './SendMailForm';
 
 export {
 	Header,
 	Main,
 	Footer,
 	MailSettings,
+	SendMailForm,
 }
